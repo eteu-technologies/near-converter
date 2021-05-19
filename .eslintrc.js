5f46090179fa3e92ab7c11eb3bdc1942ffe5aa5c
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-19 22:45:18 +0300

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
 module.exports = {
     root: true,
-
     env: {
+        es2020: true,
+        browser: true,
         node: true,
     },
-
     extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript'],
-
     parserOptions: {
         parser: '@typescript-eslint/parser',
     },
-
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
