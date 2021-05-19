c7a85070bf5c3227abc079f25039ad5f6ea50b37
Markkus Millend <markkusmillend@icloud.com>
2021-05-19 15:41:14 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "indent": ["error", 4],
+    "arrow-parens": "off",
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "space-before-function-paren": ["error", "never"],
+    "vue/html-indent": ["error", 4],
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 6,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": true
+      }
+    }],
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+  },
+};
