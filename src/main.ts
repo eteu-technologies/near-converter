cc0325fd92909f8e87794609686adeafd2c56924
Markkus Millend <markkusmillend@icloud.com>
2021-05-23 17:55:22 +0300

    Remove unused icons plugin

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { BootstrapVue } from 'bootstrap-vue';
 
 import '@/assets/app.scss';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
 
 new Vue({
     render: (h) => h(App),
